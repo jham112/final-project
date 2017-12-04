ca1a015e317ce9c456fc8e714ee2792dcb15583d
jham112 <jham112@ufl.edu>
2017-12-03 20:57:09 -0500

    Updated commit

--- js/resize.js
+++ js/resize.js
@@ -13,17 +13,16 @@ $(document).ready(function() {
   });
 
 
-    $('img').click(function(){
+    $('img').load(function(){
     	var theWidth = this.naturalWidth;
     	var theHeight = this.naturalHeight;
-    	// get a random new smaller width from the user
-    	// just to demonstrate how this works
+
     	var newWidth = 700;
-    	// calc the change from original width to new width
+
     	var thePercent = newWidth / theWidth;
-    	// apply same to height
+
     	var newHeight = theHeight * thePercent;
-    	// jQuery resizes the image based on the desired new width
+
     	$(this).css( { "width": newWidth, "height": newHeight } );
     });
 
