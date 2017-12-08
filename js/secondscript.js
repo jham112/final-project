f6f17d2922ac156d2f2373070f5a6e5aea470830
jham112 <jham112@ufl.edu>
2017-12-08 13:46:36 -0500

    Updated commit

--- js/secondscript.js
+++ js/secondscript.js
@@ -11,3 +11,16 @@ var swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
       },
     });
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("vessel").style.marginLeft = "200px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("vessel").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
